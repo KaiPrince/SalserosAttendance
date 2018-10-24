21b3c4a3a9f59648ed489e63360c689f90159a36
Kai Prince <Kprince2807@conestogac.on.ca>
2018-10-24 15:39:05 -0400

    Added attendance front-end page

--- Salseros Attendance/ClientApp/src/App.js
+++ Salseros Attendance/ClientApp/src/App.js
@@ -4,11 +4,15 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import Attendance from './components/Attendance';
+import Tutorial from './components/Tutorial';
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+    <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+        <Route path='/attendance' component={Attendance} />
+        <Route path='/test' component={Tutorial} />
+    </Layout>
 );
--- Salseros Attendance/ClientApp/src/components/NavMenu.js
+++ Salseros Attendance/ClientApp/src/components/NavMenu.js
@@ -5,31 +5,41 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export default props => (
-  <Navbar inverse fixedTop fluid collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to={'/'}>SalserosAttendance</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <LinkContainer to={'/'} exact>
-          <NavItem>
-            <Glyphicon glyph='home' /> Home
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/counter'}>
-          <NavItem>
-            <Glyphicon glyph='education' /> Counter
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/fetchdata'}>
-          <NavItem>
-            <Glyphicon glyph='th-list' /> Fetch data
-          </NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+    <Navbar inverse fixedTop fluid collapseOnSelect>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <Link to={'/'}>SalserosAttendance</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <LinkContainer to={'/'} exact>
+                    <NavItem>
+                        <Glyphicon glyph='home' /> Home
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/counter'}>
+                    <NavItem>
+                        <Glyphicon glyph='education' /> Counter
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/fetchdata'}>
+                    <NavItem>
+                        <Glyphicon glyph='th-list' /> Fetch data
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/attendance'}>
+                    <NavItem>
+                        <Glyphicon glyph='th-list' /> Attendance
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/test'}>
+                    <NavItem>
+                        <Glyphicon glyph='education' /> Tutorial
+                    </NavItem>
+                </LinkContainer>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 );
