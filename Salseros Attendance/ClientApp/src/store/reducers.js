878c7f03ed2f9f2d3544dd752ab4b461ec7d7a13
Kai Prince <Kprince2807@conestogac.on.ca>
2019-02-04 16:25:27 -0500

    Cleanup and Refactoring.

---
+++ Salseros Attendance/ClientApp/src/store/reducers.js
@@ -0,0 +1,8 @@
+/* 
+Project Name: Salseros Attendance
+File Name: reducers.js
+Programmer: Kai Prince
+Date: Feb 02, 2019
+Description: This file containes all reducers for the project. May later be separated domain.
+*/
+
