38c6457150295c690ac064e88995d4055e84f47e
Kai Prince <Kprince2807@conestogac.on.ca>
2019-02-27 14:39:02 -0500

    Added ability to show member scores.

---
+++ Salseros Attendance/Models/Score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalserosAttendance.Models
+{
+    public class Score
+    {
+        public int MemberID { get; set; }
+        public int MemberScore { get; set; }
+    }
+}
